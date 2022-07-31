cc9d3ee8aaf6345e4cfc4e13b76e9f1ba5d072eb
x <x@mail.com>
2022-07-31 04:24:12 -0400

    -

---
+++ test/TestForEach.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author asosadjpasdijasixapoapxsasxas
+ */
+public class TestForEach {
+    
+    
+    public static void main(String[] args) {
+        int edades[] = {5,6,8,9};
+        //este es un ejemplo de un for each
+        for(int edad: edades){
+            System.out.println("edad ="+edad);
+        }
+    }
+    
+}
